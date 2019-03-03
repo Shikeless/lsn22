1ee594276582163ddcfe7ad0c94855099dd0175d
Shikeless <Shikeless@github.com>
2019-03-03 21:58:22 +0300

    Collor picker add

--- app2.rb
+++ app2.rb
@@ -32,9 +32,10 @@ post '/visit' do
 	@visit_phone = params['visit_phone']
 	@visit_time = params['visit_time']
 	@visit_specialist = params['visit_specialist']
+	@visit_color = params['visit_color']
 
 	f = File.open './public/clients.txt', 'a'
-	f.write "#{@visit_name}\n#{@visit_phone}\n#{@visit_time}\n#{@visit_specialist}\n"
+	f.write "#{@visit_name}\n#{@visit_phone}\n#{@visit_time}\n#{@visit_specialist}\n#{@visit_color}"
 	f.close
 
     erb "Уважаемый #{@visit_name}, вас будет ждать #{@visit_specialist}, в #{@visit_time}, до скорого."
